49bd5ffb790af9c8c99f29bce3d3233a28c2e5f2
Davi Sanches Simão <104591817+Davisimao@users.noreply.github.com>
2023-10-09 10:43:29 -0300

    index.js

---
+++ index.js
@@ -0,0 +1,7 @@
+const elementosDuvida = document.querySelectorAll(".duvida")
+
+elementosDuvida.forEach(duvida =>{
+  duvida.addEventListener("click", function(){
+    duvida.classList.toggle("ativa")
+  }) 
+})
